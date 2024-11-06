7271f989d5088a9db75bf9ff2a36a1bef2fb8bff
AwsomeStar123456 <semrahodobasic@icloud.com>
2024-11-06 11:58:35 -0700

    Updated scheduling.c

--- src/scheduling.c
+++ src/scheduling.c
@@ -8,13 +8,13 @@
 
 #include "threads_functions.h"
 
-#define SUPERVISOR_TASK_PRIORITY      ( tskIDLE_PRIORITY + 3UL )
+#define SUPERVISOR_TASK_PRIORITY (tskIDLE_PRIORITY + 10)
 #define SUPERVISOR_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-#define WORKER_TASK_LOWER_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define WORKER_TASK_LOWER_PRIORITY (tskIDLE_PRIORITY + 3)
 #define WORKER_TASK_LOWER_STACK_SIZE configMINIMAL_STACK_SIZE
 
-#define WORKER_TASK_HIGHER_PRIORITY      ( tskIDLE_PRIORITY + 2UL )
+#define WORKER_TASK_HIGHER_PRIORITY (tskIDLE_PRIORITY + 4)
 #define WORKER_TASK_HIGHER_STACK_SIZE configMINIMAL_STACK_SIZE
 
 SemaphoreHandle_t semaphore;
@@ -23,9 +23,11 @@ void side_thread_lower(void *params)
 {
     printf("Starting lower priority thread.\n");
 
-    xSemaphoreTake(semaphore, portMAX_DELAY);
+    int status = xSemaphoreTake(semaphore, portMAX_DELAY);
 
-    printf("Lowe priority thread has acquired the semaphore.\n");
+    if(status == pdTRUE) {
+        printf("Lower priority thread has acquired the semaphore.\n");
+    }
 
     int count = 0;
 
@@ -33,10 +35,12 @@ void side_thread_lower(void *params)
         printf("Lower Priority Executing. Current Count: %d\n", count);
         vTaskDelay(1000);
 
-        if(count = 100) {
+        if(count == 100) {
             xSemaphoreGive(semaphore);
             return;
         }
+
+        count++;
 	}
 }
 
@@ -44,9 +48,11 @@ void side_thread_higher(void *params)
 {
     printf("Starting higher priority thread.\n");
 
-    xSemaphoreTake(semaphore, portMAX_DELAY);
+    int status = xSemaphoreTake(semaphore, portMAX_DELAY);
 
-    printf("Higher priority thread has acquired the semaphore.\n");
+    if(status == pdTRUE) {
+        printf("Higher priority thread has acquired the semaphore.\n");
+    }
 
     int count = 0;
 
@@ -54,33 +60,40 @@ void side_thread_higher(void *params)
         printf("Higher Priority Executing. Current Count: %d\n", count);
         vTaskDelay(1000);
 
-        if(count = 100) {
+        if(count == 100) {
             xSemaphoreGive(semaphore);
             return;
         }
+
+        count++;
 	}
 }
 
 void supervisor_thread(void *params)
 {
     semaphore = xSemaphoreCreateBinary();
+    xSemaphoreGive(semaphore);
 
     TaskHandle_t lower_worker, higher_worker;
 
     xTaskCreate(side_thread_lower, "LowerWorker",
         WORKER_TASK_LOWER_STACK_SIZE, NULL, WORKER_TASK_LOWER_PRIORITY, &lower_worker);
 
-    vTaskDelay(1000);
+    vTaskDelay(10000);
+
+    printf("Starting High Priority Thread.\n");
 
     xTaskCreate(side_thread_higher, "HigherWorker",
         WORKER_TASK_HIGHER_STACK_SIZE, NULL, WORKER_TASK_HIGHER_PRIORITY, &higher_worker);
+
+    vTaskDelay(10000);
 
 }
 
 int main(void)
 {
     stdio_init_all();
-    sleep_ms(5000);
+    sleep_ms(10000);
     TaskHandle_t supervisor_handler;
     xTaskCreate(supervisor_thread, "SupervisorThread",
                 SUPERVISOR_TASK_STACK_SIZE, NULL, SUPERVISOR_TASK_PRIORITY , &supervisor_handler);
